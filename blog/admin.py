9ca6d50ccbc9b17c688f2d854d6057e329c1f847
Mars Margoolis <mars.margoolis@gmail.com>
2016-02-06 18:27:28 +0300

    Name HSE

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.site_header = 'Суперсайт бизнес-инкубатора ВШЭ'
-admin.site.site_title = 'Суперсайт бизнес-инкубатора ВШЭ'
-admin.site.index_title = 'Суперсайт бизнес-инкубатора ВШЭ'
+admin.site.site_header = 'margoolis'
+admin.site.site_title = 'margoolis'
+admin.site.index_title = 'margoolis'
 
 admin.site.register(Post)
