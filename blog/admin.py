8a37dc56bfe1da78c8bd3744f50c5a8e26c01b5c
Mars Margoolis <mars.margoolis@gmail.com>
2016-02-06 17:27:33 +0300

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
+# coding: utf-8
+
 from django.contrib import admin
 from .models import Post
+
+admin.site.site_header = 'Суперсайт бизнес-инкубатора ВШЭ'
+admin.site.site_title = 'Суперсайт бизнес-инкубатора ВШЭ'
+admin.site.index_title = 'Суперсайт бизнес-инкубатора ВШЭ'
 
 admin.site.register(Post)
